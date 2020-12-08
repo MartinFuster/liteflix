c91372accfab96613187a056c4faddc56a179362
MartinFuster <martinfuster3@gmail.com>
2020-12-08 00:46:47 -0300

    category selection now works, pre-drag and drop state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import plus from "./images/plus-icon.svg";
 import SmallPreview from './components/SmallPreview';
 import LargePreview from './components/LargePreview';
 import axios from "axios";
+import Modal from "./components/Modal";
 import clip from "./images/clip.svg";
 
 function App() {
@@ -123,6 +124,7 @@ function App() {
 
   function addMovieExit() {
     setAddMovieActive(false);
+    handleCategoryExit();
     const app = document.getElementById("App");
     document.documentElement.style.height = "100%";
     document.documentElement.style.overflowY = "auto";
@@ -135,109 +137,7 @@ function App() {
         <animated.div className="popup-shadow" style={addMovieAnimation} onClick={() => addMovieExit()}></animated.div>
         <div className="container">
         <Navbar addMovieOpen={addMovieOpen} />
-        <animated.div className="modal-container" style={addMovieAnimation} onClick={() => handleCategoryExit()}>
-          <div className="cross" onClick={() => addMovieExit()}>&times;</div>
-            <div className="dropzone">
-                <input className="disabled-input"/>
-                <p className="dropzone-text">
-                    <img src={clip} alt="Agregar Archivo" className="clip"/>
-                    <span className="dropzone-text-highlight">Agregar Archivo</span> o arrastrarlo y soltarlo aquí
-                </p>
-            </div>
-            <div className="flex flex-wrap">
-                <div className="modal-input-container u-margin-right-3">
-                    <div className="modal-input-title">NOMBRE DE LA PELÍCULA</div>
-                    <input type="text" className="modal-input"/>
-                </div>
-                <div className="modal-input-container">
-                    <div className="modal-input-title">CATEGORIA</div>
-                    <input type="text" className="modal-input modal-input-option" value={""} onClick={() => handleCategoryOpen()} />
-                    <div className="modal-input-category-expanded" id="categoryExpanded">
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Acción</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Animación</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Aventura</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Ciencia Ficción</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Comedia</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Documental</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Crimen</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Drama</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Familia</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Fantasía</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Historia</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Terror</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Música</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Misterio</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Romance</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Pelicula de TV</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Suspenso</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Bélica</div>
-                        <hr className="category-hr"/>
-                      </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
-                        <div className="category-name">Western</div>
-                        <hr className="category-hr"/>
-                      </div>
-                    </div>
-                </div>
-            </div>
-            <div className="button-container">
-              <button type="submit" className="modal-btn">
-                  Subir Película
-              </button>
-            </div>
-        </animated.div>
+        <Modal addMovieActive={addMovieActive} addMovieExit={addMovieExit} />
         </div>
           <div className="container align-center-container">
             <div className="movie-info-box">
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSpring, animated } from "react-spring";
-import clip from "./images/clip.svg";
+import clip from "../images/clip.svg";
 
-const addMovieAnimation = useSpring({
+function Modal(props) {
+    
+  const [addMovieActive, setAddMovieActive] = useState(false);
+  const [movieCategory, setMovieCategory] = useState("");
+
+  useEffect(() => {
+    setAddMovieActive(props.addMovieActive);
+  }, [props.addMovieActive]);
+
+  const addMovieAnimation = useSpring({
     opacity: addMovieActive ? 1 : 0,
     visibility: addMovieActive ? "visible" : "hidden",
   });
 
-function Modal() {
+  function handleCategoryOpen() {
+    const categoryExpanded = document.getElementById("categoryExpanded");
+    setTimeout(() => {
+      categoryExpanded.style.visibility = "visible";
+    }, 50);
+    categoryExpanded.style.opacity = 1;
+  }
+
+  function handleCategorySelection(category) {
+    setMovieCategory(category);
+    handleCategoryExit();
+  }
+
+  function handleCategoryExit() {
+    const categoryExpanded = document.getElementById("categoryExpanded");
+    if (categoryExpanded.style.visibility === "visible") {
+      categoryExpanded.style.opacity = 0;
+      setTimeout(() => {
+        categoryExpanded.style.visibility = "hidden";
+      }, 200);
+    }
+  }
+
+  function addMovieExit() {
+    setAddMovieActive(false);
+    handleCategoryExit();
+    props.addMovieExit();
+  }
+  
     return(
         <animated.div className="modal-container" style={addMovieAnimation} onClick={() => handleCategoryExit()}>
           <div className="cross" onClick={() => addMovieExit()}>&times;</div>
@@ -25,81 +62,81 @@ function Modal() {
                 </div>
                 <div className="modal-input-container">
                     <div className="modal-input-title">CATEGORIA</div>
-                    <input type="text" className="modal-input modal-input-option" value={""} onClick={() => handleCategoryOpen()} />
+                    <input type="text" className="modal-input modal-input-option" value={movieCategory} onClick={() => handleCategoryOpen()} />
                     <div className="modal-input-category-expanded" id="categoryExpanded">
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Acción")}>
                         <div className="category-name">Acción</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Animación")}>
                         <div className="category-name">Animación</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Aventura")}>
                         <div className="category-name">Aventura</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Ciencia Ficción")}>
                         <div className="category-name">Ciencia Ficción</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Comedia")}>
                         <div className="category-name">Comedia</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Documental")}>
                         <div className="category-name">Documental</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Crimen")}>
                         <div className="category-name">Crimen</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Drama")}>
                         <div className="category-name">Drama</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Familia")}>
                         <div className="category-name">Familia</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Fantasía")}>
                         <div className="category-name">Fantasía</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Historia")}>
                         <div className="category-name">Historia</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Terror")}>
                         <div className="category-name">Terror</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Música")}>
                         <div className="category-name">Música</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Misterio")}>
                         <div className="category-name">Misterio</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Romance")}>
                         <div className="category-name">Romance</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Pelicula de TV")}>
                         <div className="category-name">Pelicula de TV</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Suspenso")}>
                         <div className="category-name">Suspenso</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Bélica")}>
                         <div className="category-name">Bélica</div>
                         <hr className="category-hr"/>
                       </div>
-                      <div className="category-container" onClick={() => handleCategoryExit()}>
+                      <div className="category-container" onClick={() => handleCategorySelection("Western")}>
                         <div className="category-name">Western</div>
                         <hr className="category-hr"/>
                       </div>
